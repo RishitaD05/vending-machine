bb72884b7aac189a7b74d4c17fc8a3e3a4566afa
RishitaD05 <deyrishita64@gmail.com>
2025-03-02 23:20:01 +0530

    Update vending.v

--- vending.v
+++ vending.v
@@ -6,13 +6,13 @@ module vending(
     input [1:0]choice,
     
     output reg[4:0]change,
-    output reg howrah, manikaran, esplanade
+    output reg howrah,manikaran,esplanade
     );
     //the states are defined as follows
-    parameter [2:0]zero=3'b000;
-    parameter [2:0]five=3'b001;
-    parameter [2:0]ten=3'b010;
-    parameter [2:0]fifteen=3'b011;
+    parameter [1:0]zero=2'b00;
+    parameter [1:0]ten=2'b01;
+    parameter [1:0]fifteen=2'b10;
+    parameter [1:0]twenty=2'b11;
     
     //the choices are as follows:
     parameter [1:0]sel_howrah=2'b01;
@@ -20,9 +20,9 @@ module vending(
     parameter [1:0]sel_esplanade=2'b11;
     
     //the denominations are as follows:
-    parameter [4:0]m_5=5'd5;
     parameter [4:0]m_10=5'd10;
-    parameter [4:0]m_20=5'd20;
+    parameter [4:0]m_15=5'd15;
+    parameter [4:0]m_25=5'd25;
     
     reg [2:0]c_s,n_s;
     initial
@@ -33,108 +33,109 @@ module vending(
     always @(posedge clk)
      
         if(rst) begin
-          c_s<=zero;
-          n_s<=zero;
+         c_s <=0;
+          n_s<=0;
           end
-        else begin 
-        c_s=n_s;
+        else begin
+           c_s=n_s;
            case(c_s)
-             zero: if(in==m_5 && choice==sel_howrah)
-                     n_s<= five;
-                
-                   else if( in==m_10 && choice==sel_manikaran)
+             zero: if(in==m_10 && choice==sel_howrah)
                      n_s<= ten;
-                 
-                   else if( in==m_20 && choice==sel_esplanade)
+                
+                   else if( in==m_15 && choice==sel_manikaran)
                      n_s<= fifteen;
+                 
+                   else if( in==m_25 && choice==sel_esplanade)
+                     n_s<= twenty;
                      
-              five: if(in==m_5 && choice==sel_howrah)
-                     n_s<= five;
-                
-                   else if( in==m_10 && choice==sel_manikaran)
+              ten: if(in==m_10 && choice==sel_howrah)
                      n_s<= ten;
-                 
-                   else if( in==m_20 && choice==sel_esplanade)
-                     n_s<= fifteen;
-             ten: if(in==m_5 && choice==sel_howrah)
-                     n_s<= five;
                 
-                   else if( in==m_10 && choice==sel_manikaran)
-                     n_s<= ten;
-                 
-                   else if( in==m_20 && choice==sel_esplanade)
+                   else if( in==m_15 && choice==sel_manikaran)
                      n_s<= fifteen;
+                 
+                   else if( in==m_25 && choice==sel_esplanade)
+                     n_s<= twenty;
+             fifteen: if(in==m_10 && choice==sel_howrah)
+                     n_s<= ten;
+                
+                   else if( in==m_15 && choice==sel_manikaran)
+                     n_s<= fifteen;
+                 
+                   else if( in==m_25 && choice==sel_esplanade)
+                     n_s<= twenty;
               
-             fifteen: if(in==m_5 && choice==sel_howrah)
-                     n_s<= five;
-                
-                   else if( in==m_10 && choice==sel_manikaran)
+             twenty: if(in==m_10 && choice==sel_howrah)
                      n_s<= ten;
-                 
-                   else if( in==m_20 && choice==sel_esplanade)
+                
+                   else if( in==m_15 && choice==sel_manikaran)
                      n_s<= fifteen;
+                 
+                   else if( in==m_25 && choice==sel_esplanade)
+                     n_s<= twenty;
                      
                default: n_s<= zero;
                
 endcase
 end
  always @(posedge clk)
-     
      begin
+          
         howrah <= 1'b0;
         manikaran <= 1'b0;
         esplanade <= 1'b0;
+
          case(c_s)
            zero:begin
-                  howrah <=1'b0;
-                  manikaran <=1'b0;
-                  esplanade <=1'b0;
+                  howrah<=1'b0;
+                  manikaran<=1'b0;
+                  esplanade<=1'b0;
                   change=in; 
                   end 
-            five: begin
-                if(in==m_5)
+            ten: begin
+                if(in==m_10)
                  begin
                    howrah<=1'b1;
                    manikaran<=1'b0;
                    esplanade<=1'b0;
-                   change=in-5; 
+                   change=in-10; 
                   end 
                     
-               else if( in==m_10)
+               else if( in==m_15)
                   begin
                    howrah<=1'b1;
                    manikaran<=1'b0;
                    esplanade<=1'b0;
-                   change=in-5; 
+                   change=in-10; 
                   end 
                  
-                else if( in==m_20)
+                else if( in==m_25)
                   begin
-                   howrah <=1'b1;
-                   manikaran <=1'b0;
-                   esplanade <=1'b0;
-                   change=in-5; 
+                   howrah<=1'b1;
+                   manikaran<=1'b0;
+                   esplanade<=1'b0;
+                   change=in-10; 
                   end 
                 end      
               
-             ten: begin
-                if(in==m_5)
+              fifteen: begin
+                if(in==m_10)
                  begin
-                   howrah <=1'b0;
-                   manikaran <=1'b0;
-                   esplanade <=1'b0;
+                   howrah<=1'b0;
+                   manikaran<=1'b0;
+                   esplanade<=1'b0;
                    
-                   if(extra_cash==m_5)
+                   if(extra_cash==m_10)
                      begin
-                       howrah <=1'b0;
-                       manikaran <=1'b1;
-                      esplanade <=1'b0;
-                      
+                       howrah<=1'b0;
+                       manikaran<=1'b1;
+                      esplanade<=1'b0;
+                      change=in-5;
                      end 
-                     else change=5'd5;
+                     else change=5'd10;
                   end 
                     
-               else if( in==m_10)
+               else if( in==m_15)
                   begin
                    howrah<=1'b0;
                    manikaran<=1'b1;
@@ -142,7 +143,7 @@ end
                    change=in-10; 
                   end 
                  
-                else if( in==m_20)
+                else if( in==m_25)
                   begin
                    howrah<=1'b0;
                    manikaran<=1'b1;
@@ -150,8 +151,8 @@ end
                    change=in-10; 
                   end 
                 end      
-               fifteen: begin
-                if(in==m_5)
+               twenty: begin
+                if(in==m_10)
                  begin
                    howrah<=1'b0;
                    manikaran<=1'b0;
@@ -164,33 +165,33 @@ end
                        esplanade<=1'b1;
                       
                      end 
-                     else change=5'd5;
+                     else change=5'd10;
                   end 
                     
-               else if( in==m_10)
+               else if( in==m_15)
                   begin
                    howrah<=1'b0;
                    manikaran<=1'b0;
                    esplanade<=1'b0;
                    
-                     if(extra_cash==m_5)
+                     if(extra_cash==m_10)
                      begin
                        howrah<=1'b0;
                        manikaran<=1'b0;
                        esplanade<=1'b1;
-                       
+                       change=5'd5;
                       
                      end 
-                     else change=5'd10;
+                     else change=5'd15;
                       
                   end 
                  
-                else if( in==m_20)
+                else if( in==m_25)
                   begin
                    howrah<=1'b0;
                    manikaran<=1'b0;
                    esplanade<=1'b1;
-                   change=in-15; 
+                   change=in-20; 
                   end
                  end
                   default: begin
@@ -199,6 +200,6 @@ end
                        esplanade<=1'b0;
                        change=5'd0;
                       end
-                     endcase 
-                end      
- endmodule
+                     endcase
+                     end
+        endmodule
